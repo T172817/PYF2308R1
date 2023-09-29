12ee11d9be1f43d9c09c62007c6f86447060fc04
TungTNT <tungsmell911@gmail.com>
2023-09-29 14:09:20 +0700

    Bai thuc hanh ve hinh chu nhat

---
+++ PYF-W01/DrawCircle.py
@@ -0,0 +1,21 @@
+import turtle
+t = turtle.Turtle()
+
+m = 100;
+n = 150;
+ 
+# vẽ cạnh đầu tiên
+t.forward(m) 
+t.left(90)   
+ 
+# vẽ cạnh thứ hai
+t.forward(n) 
+t.left(90)   
+ 
+# vẽ cạnh thứ ba
+t.forward(m) 
+t.left(90)   
+ 
+# vẽ cạnh thứ tư
+t.forward(n) 
+t.left(90)   
