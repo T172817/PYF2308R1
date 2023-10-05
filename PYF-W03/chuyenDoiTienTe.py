b529160e7219f2767bc70df3353413703135959e
TungTNT <tungsmell911@gmail.com>
2023-10-05 16:27:54 +0700

    Them folder W03

---
+++ PYF-W03/chuyenDoiTienTe.py
@@ -0,0 +1,4 @@
+tienVaoUSD = int(input("Nhap vao so tien (USD) can doi: "))
+tyGia = float(input("Nhap vao ty gia USD/VND: "))
+soTienVND = tienVaoUSD * tyGia
+print(f"So tien VND theo ti gia: {soTienVND}")
