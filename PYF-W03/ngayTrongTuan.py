7f907e2a88c1848b0934decf082bb531065de0b8
TungTNT <tungsmell911@gmail.com>
2023-10-18 15:30:14 +0700

    Up bai hien thi ngay

---
+++ PYF-W03/ngayTrongTuan.py
@@ -0,0 +1,17 @@
+soNgay = int(input("Nhap vao ngay dang so: "))
+if soNgay == 1:
+    print("Monday")
+elif soNgay == 2:
+    print("Tuesday")
+elif soNgay == 3:
+    print("Wednesday")
+elif soNgay == 4:
+    print("Thursday")
+elif soNgay == 5:
+    print("Sunday")
+elif soNgay == 6:
+    print("Satuday")
+elif soNgay == 6:
+    print("Sunday")
+else:
+    print("error, out of range")
